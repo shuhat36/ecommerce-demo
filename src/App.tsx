1efe61b62f70b955001883bd7f0ae3016a9d3be6
shuhat36 <shuhat.uddin@selise.ch>
2024-03-22 13:14:53 +0600

    MOD: react-router added for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Products from "./pages/Products";
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <Products />
+      <ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Products />} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
+      </ChakraProvider>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
